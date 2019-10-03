de93c7643e0a1dd984a59ccadd0e65c6abe49836
Rodrigo Mendes <ro.mendes1989@gmail.com>
2019-10-03 23:42:53 +0200

    create project

---
+++ build/precache-manifest.6b92a1611a4e21219ad05a790062284b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299d809d501f3b417c1d62f159d7ad30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c4ec4db2d81e8cc7e85",
+    "url": "/static/css/main.3e9d6df6.chunk.css"
+  },
+  {
+    "revision": "10f64321900779a83b30",
+    "url": "/static/js/2.680d54fd.chunk.js"
+  },
+  {
+    "revision": "4c4ec4db2d81e8cc7e85",
+    "url": "/static/js/main.94feebe8.chunk.js"
+  },
+  {
+    "revision": "65a81b1ca9a7620b8ceb",
+    "url": "/static/js/runtime-main.1dc4db23.js"
+  },
+  {
+    "revision": "83252e0afaf5f134063345abd6ac8fd7",
+    "url": "/static/media/background.83252e0a.jpg"
+  },
+  {
+    "revision": "6a6f4b6ebebfabdc283a3a6b417282a0",
+    "url": "/static/media/camera.6a6f4b6e.svg"
+  },
+  {
+    "revision": "740528028131fdef515dcb930373ccf6",
+    "url": "/static/media/logo.74052802.svg"
+  }
+]);
